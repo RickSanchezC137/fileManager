95b3fe1e63d73ad7bb4ad1af09d77cce8760ce5b
RickSanchezC137 <tileslala@gmail.com>
2017-05-19 11:57:29 +0530

    MySQL database added

--- bowser/bowser/urls.py
+++ bowser/bowser/urls.py
@@ -1,4 +1,4 @@
-"""fileBrowser URL Configuration
+"""bowser URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls import include
 
 urlpatterns = [
-	url(r'^polls/', include('polls.urls')),
+    url(r'^fileBrowser/', include('fileBrowser.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- bowser/bowser/wsgi.py
+++ bowser/bowser/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fileBrowser project.
+WSGI config for bowser project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fileBrowser.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bowser.settings")
 
 application = get_wsgi_application()
